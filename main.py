2e7fc06fd6cbb31198ef8338e686562b6e4f1b61
Kameil <130675534+Kameil@users.noreply.github.com>
2023-09-29 16:42:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -20,23 +20,8 @@ for i in range(101):
 poketwo, pokename, paused, token = '716390085896962058', '0', False, os.environ['token']
 unidentified_image = 'original_image.png'
 
-with open('data/pokemon', 'r', encoding='utf8') as file:
-    pokemon_list = file.read()
 
-# Configuração do bot
 bot = commands.Bot(command_prefix='.', self_bot=True, help_command=None)
-
-def solve(message):
-    hint = []
-    for i in range(15, len(message) - 1):
-        if message[i] != '\\':
-            hint.append(message[i])
-    hint_string = ''
-    for i in hint:
-        hint_string += i
-    hint_replaced = hint_string.replace('_', '.')
-    solution = re.findall('^' + hint_replaced + '$', pokemon_list, re.MULTILINE)
-    return solution
 
 @bot.event
 async def on_ready():
